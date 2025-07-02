e1ae56b6c54e4ae3b4b09062fe7cbcb5500ea131
Immanuvel B <101741564+Inman2004@users.noreply.github.com>
2025-07-02 15:02:51 +0530

    small

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -10,17 +10,6 @@ export default function BlogLayout({
   return (
     <>
       {children}
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            background: '#1a1a1a',
-            color: '#fff',
-            border: '1px solid #333',
-            borderRadius: '8px',
-          },
-        }}
-      />
     </>
   );
 }
